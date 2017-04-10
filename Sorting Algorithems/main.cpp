8f34b42d49f0adbcd7e7b251afae418318d5b96b
Mohammed Jacksi <mohjacksi@mohammeds-macbook-air.local>
2017-04-10 22:51:54 +0300

    Initial Commit

---
+++ Sorting Algorithems/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Sorting Algorithems
+//
+//  Created by Mohammed Jacksi on 4/10/17.
+//  Copyright © 2017 Mohammed Jacksi. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
